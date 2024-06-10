e4f4257bcd7cc385df5908409857250bc688777d
SamGiKim <rlawns256@naver.com>
2024-06-10 18:20:00 +0900

    add exam1173, 1180
    add exam1173, 1180

--- study01/src/main/java/com/studymavenspringboot1/study01/mathexam/Main.java
+++ study01/src/main/java/com/studymavenspringboot1/study01/mathexam/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) {
         try{
            //exam1566();
+            exam1173();
         } catch (Exception e){
             e.printStackTrace();
         }
@@ -31,6 +32,59 @@ public class Main {
             System.out.println("Leap");
         } else{
             System.out.println("Normal");
+        }
+    }
+
+    public static void exam1165(){
+        Scanner sc = new Scanner(System.in);
+        int time = sc.nextInt();
+        int twoban = sc.nextInt();
+
+        for(int i = time; i<90; i+=5){
+            twoban++;
+        }
+        System.out.println(twoban);
+    }
+
+    public static void exam1173() throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int hour = sc.nextInt();
+        int mins = sc.nextInt();
+
+        if(hour<0 || hour >= 24){
+            throw new Exception(String.format("시간(%d)은 0~24 이어야 합니다.", hour));
+        } else if(mins<0 || mins >= 60){
+            throw new Exception(String.format("시간(%d)은 0~59 이어야 합니다.", mins));
+        }
+
+        mins -= 30;
+        if(mins<0){
+            if(hour <= 0){
+                hour = 23;
+            } else {
+                hour --;
+            }
+            mins += 60;
+        }
+        System.out.println(hour + " " + mins);
+    }
+
+    public static void exam1180(){
+        Scanner sc = new Scanner(System.in);
+        int trash = sc.nextInt();
+        int num1 = trash / 10;
+        int num2 = trash % 10;
+        int result = (num2*10 + num1) * 2;
+
+        if(result >= 100){
+            result -= 100;
+        }
+
+        System.out.println(result);
+        if(result <= 50){
+            System.out.println("GOOD");
+        } else {
+            System.out.println("OH MY GOD");
         }
     }
 }
