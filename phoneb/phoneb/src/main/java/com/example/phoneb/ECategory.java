31c6f39de5c005ac3c596ed5fc79aa266f10dc2e
SamGiKim <rlawns256@naver.com>
2024-07-05 15:16:56 +0900

    ECategory 인덱스로 입력하기
    ECategory 인덱스로 입력하기

--- phoneb/phoneb/src/main/java/com/example/phoneb/ECategory.java
+++ phoneb/phoneb/src/main/java/com/example/phoneb/ECategory.java
@@ -1,9 +1,27 @@
 package com.example.phoneb;
 
 public enum ECategory {
-    Friends,
-    Families,
-    Schools,
-    Jobs,
-    Hobbies
+    Friends(0),
+    Families(1),
+    Schools(2),
+    Jobs(3),
+    Hobbies(4);
+
+    private final Integer val;
+    ECategory(Integer val){
+        this.val=val;
+    }
+    public Integer getVal(){
+        return this.val;
+    }
+
+    private static final ECategory[] ECategoryArray = ECategory.values();
+    public static ECategory integerOf(Integer val){
+        for(ECategory item : ECategoryArray){
+            if(item.getVal() == val){
+                return item;
+            }
+        }
+        throw new IllegalArgumentException("ECategory value : " + val);
+    }
 }
