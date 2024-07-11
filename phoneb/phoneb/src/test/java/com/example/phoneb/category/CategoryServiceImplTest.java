90cb51d33039f4a4b904d77300aad57ddbb7f39a
SamGiKim <rlawns256@naver.com>
2024-07-11 14:40:30 +0900

    add Test
    add Test

--- phoneb/phoneb/src/test/java/com/example/phoneb/category/CategoryServiceImplTest.java
+++ phoneb/phoneb/src/test/java/com/example/phoneb/category/CategoryServiceImplTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
